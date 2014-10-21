639ea9a3bd1b9de9a87c3fea173e2aa0dfa7b929
Isabel Dominik <icd107@live.com>
2014-10-21 16:10:46 -0700

    Made window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	return 0;
+	sf::RenderWindow window (sf::VideoMode (800, 600), "SFML window");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
+
 }
