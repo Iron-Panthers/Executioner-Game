a3187c722302b7c495e6d7d57fe9fd551dcec4a2
Isabel Dominik <icd107@live.com>
2014-10-23 16:16:42 -0700

    started texture manager class

---
+++ src/TextureManager.cpp
@@ -0,0 +1,6 @@
+#include "TextureManager.hpp"
+
+TextureManager::TextureManager()
+{
+	
+}
---
+++ src/TextureManager.hpp
@@ -0,0 +1,19 @@
+#ifndef TEXTUREMANAGER_HPP
+#define TEXTUREMANAGER_HPP
+#include <vector>
+#include <utility>
+#include <map>
+
+class TextureManager
+{
+public:
+	void getTexture();
+
+
+private:
+	std::map
+
+
+};
+
+#endif
